d4857cd708a8bb76d7eac371918d561a8551c5df
touretta <150069411+touretta@users.noreply.github.com>
2024-11-04 22:54:05 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
+
+import "package:flutter/material.dart";
+import "package:flutter_demo/pages/first_page.dart";
+import 'package:flutter_demo/pages/registration_page.dart';
+import 'package:flutter_demo/pages/bunkers_page.dart';
+import 'package:flutter_demo/pages/zayavki_page.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(),
+      routes: {
+        '/firstpage': (context) => FirstPage(),
+        '/registrationpage': (context) => RegistrationPage(),
+        '/bunkerspage' : (context) => BunkersPage(),
+        '/zayavkipage' : (context) => ZayavkiPage(),
+      }
+      );
+  }
+}
+
+
+
