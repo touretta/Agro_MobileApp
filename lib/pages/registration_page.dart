856ac5e1ff4bbd066c13b01c7ead90a0c14f7751
touretta <150069411+touretta@users.noreply.github.com>
2024-11-05 14:13:26 +0300

    up to date lib 05.11.2024
    zayavka and registration template

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -1,10 +1,40 @@
 import "package:flutter/material.dart";
 
 class RegistrationPage extends StatelessWidget {
-  const RegistrationPage({super.key});
+  RegistrationPage({super.key});
+
+TextEditingController myLogin = TextEditingController();
+TextEditingController myPassword = TextEditingController();
+
+void greetUser() {
+  print(myLogin.text);
+  print(myPassword.text);
+}
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+        TextField(
+          controller: myLogin,
+          decoration: InputDecoration(
+            labelText: 'почта',
+            border: OutlineInputBorder()
+          ),
+        ),
+        TextField(
+          controller: myPassword,
+          decoration: InputDecoration(
+            labelText: 'пароль',
+            border: OutlineInputBorder()
+          ),
+        ),
+        ElevatedButton(onPressed: greetUser, child: Text('Tap!'),)
+      ],)),)
+    );
   }
 }
--- lib/pages/zayavki_page.dart
+++ lib/pages/zayavki_page.dart
@@ -1,10 +1,57 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import "package:flutter/material.dart";
 
 class ZayavkiPage extends StatelessWidget {
   const ZayavkiPage ({super.key});
 
+void greetUser() {
+  print("hai");
+}
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+        TextField(
+  //        controller: myLogin,
+          decoration: InputDecoration(
+            labelText: 'Mesto_Vigruzki',
+            border: OutlineInputBorder()
+          ),
+        ),
+        TextField(
+  //        controller: myLogin,
+          decoration: InputDecoration(
+            labelText: 'Value_Vigruzki',
+            border: OutlineInputBorder()
+          ),
+        ),
+        TextField(
+  //        controller: myLogin,
+          decoration: InputDecoration(
+            labelText: 'Zapolnenost_bunkerov',
+            border: OutlineInputBorder()
+          ),
+        ),
+        TextField(
+  //        controller: myLogin,
+          decoration: InputDecoration(
+            labelText: 'Vremya_Vigruzki',
+            border: OutlineInputBorder()
+          ),
+        ),
+        TextField(
+ //         controller: myPassword,
+          decoration: InputDecoration(
+            labelText: 'FIO',
+            border: OutlineInputBorder()
+          ),
+        ),
+        ElevatedButton(onPressed:greetUser, child: Text('Tap!'),)
+      ],)),));
   }
 }
